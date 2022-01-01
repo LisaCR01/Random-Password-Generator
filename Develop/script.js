3e7d8f0c60ddd76ca06b3ec3f57203bccc7d4346
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-01 15:02:21 +0000

    added in prompts for the characteristics of the password and also added a loop on the password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var check=false
 alert("Hello this is your friendly password generator")
+
+// ask for password length
+//and check it is 8-128 long, 
+// and an integer
+//loop until all these conditions are true.
+
+do{
 let pwdLength=prompt("How long would you like your password (8-128)?")
+pwdLength=Number(pwdLength)
+
+if(Number.isInteger(pwdLength)){alert ("yes integer");check=true}
+else {alert("not integer")}
+if(pwdLength>=8 && pwdLength<=128){alert("length ok")}
+
+else {alert ("length wrong");check=false}
+}
+while(check==false)
+
+let pwdLowercase=prompt("Would you like your password to include lowercase yes/no")
+if (pwdLowercase=="yes"){alert("password will include lowercase")}
+else {alert ("password will not include lowercase")}
+
+let pwdUppercase=prompt ("Would you like your password to include uppercase yes/no")
+if (pwdUppercase=="yes"){alert("password will include uppercase")}
+else {alert ("password will not include uppercase")}
+
+let pwdNumbers=prompt ("Would you like your password to include numbers yes/no")
+if (pwdNumbers=="yes"){alert("password will include numbers")}
+else {alert ("password will not include numbers")}
+
+let pwdSpecial=prompt ("Would you like your password to include special characters yes/no")
+if (pwdSpecial=="yes"){alert("password will include special character")}
+else {alert ("password will not include special characters")}
+
+
 // Write password to the #password input
 function writePassword() {
   //var password = generatePassword();
