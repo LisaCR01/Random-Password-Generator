4ce0097b499b45749a67b42b6c104daeb93a535f
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-02 15:26:06 +0000

    generated random password

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var check=false
+var pwdL=0
 alert("Hello this is your friendly password generator")
 
 // ask for password length
@@ -9,31 +10,32 @@ alert("Hello this is your friendly password generator")
 //loop until all these conditions are true.
 
 do{
-let pwdLength=prompt("How long would you like your password (8-128)?")
+  let pwdLength=prompt("How long would you like your password (8-128)?")
 pwdLength=Number(pwdLength)
 
 if(Number.isInteger(pwdLength)){alert ("yes integer");check=true}
 else {alert("not integer")}
-if(pwdLength>=8 && pwdLength<=128){alert("length ok")}
+if(pwdLength>=8 && pwdLength<=128){alert("length ok");pwdL=pwdLength}
 else {alert ("length wrong");check=false}
 }
 while(check==false)
-
-let pwdLowercase=prompt("Would you like your password to include lowercase yes/no")
+console.log("pwd "+ pwdL)
+let pwdLowercase=prompt("Would you like your password to include lowercase yes/no","yes")
 if (pwdLowercase=="yes"){alert("password will include lowercase")}
 else {alert ("password will not include lowercase")}
 
-let pwdUppercase=prompt ("Would you like your password to include uppercase yes/no")
+let pwdUppercase=prompt ("Would you like your password to include uppercase yes/no","yes")
 if (pwdUppercase=="yes"){alert("password will include uppercase")}
 else {alert ("password will not include uppercase")}
 
-let pwdNumbers=prompt ("Would you like your password to include numbers yes/no")
+let pwdNumbers=prompt ("Would you like your password to include numbers yes/no","yes")
 if (pwdNumbers=="yes"){alert("password will include numbers")}
 else {alert ("password will not include numbers")}
 
-let pwdSpecial=prompt ("Would you like your password to include special characters yes/no")
+let pwdSpecial=prompt ("Would you like your password to include special characters yes/no","yes")
 if (pwdSpecial=="yes"){alert("password will include special character")}
 else {alert ("password will not include special characters")}
+//STILL need to put in a check that one of these is a yes. 
 
 //we need to create an array of the characters that can be included in the password, incChars.
 let incChars=""
@@ -47,7 +49,16 @@ if(pwdUppercase=="yes"){incChars+=upperAlphabet}
 if(pwdNumbers=="yes"){incChars+=numbers}
 if(pwdSpecial=="yes"){incChars+=specialCharacters}
 
+//generate random integers and use that to find an element of the array
 
+
+let pwdGen=""
+for (let i = 0; i < pwdL; i++){
+  pwdGen+=incChars[Math.floor(Math.random() * incChars.length)]
+  console.log("watch the password generate "+ pwdGen)
+}
+
+incChars[Math.floor(Math.random() * incChars.length)]
 
 // Write password to the #password input
 function writePassword() {
